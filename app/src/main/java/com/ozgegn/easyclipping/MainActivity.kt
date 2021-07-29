9fbe2792df1a289247935a3ec821be1b5fa9ebc5
ozgegn <ozgegundogduu92@gmail.com>
2021-07-29 18:52:29 +0300

    initial commit

---
+++ app/src/main/java/com/ozgegn/easyclipping/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ozgegn.easyclipping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(ClippingView(this))
+    }
+}
